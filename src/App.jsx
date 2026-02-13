3e717726b4d8884b7c9015f71ab2bf616a01f1ed
jkhiba2023 <khan.jabir1055@gmail.com>
2026-02-13 17:29:33 +0530

    DOne with signup component

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/signin" element={<Signin />} />
-        <Route path="/login" element={<Login/>}
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
