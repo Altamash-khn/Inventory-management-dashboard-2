bc8920c6c09927d9f33297b0a6054cbb5f75f844
Gulam Mohammad Khan <gulamkhan512@gmail.com>
2026-02-13 19:46:01 +0530

    login-responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Signin from "./Components/Signin/Signin";
+import Login from "./Components/Login/Login";
 
 function App() {
   return (
@@ -9,14 +10,6 @@ function App() {
         <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
-import Login from "./Components/Login/Login";
-
-function App() {
-  return (
-    <div className="bg-[#090b0f]">
-      <Login />
-    </div>
   );
 }
-
 export default App;
